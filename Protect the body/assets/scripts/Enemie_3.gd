205b2d314f107901373f384f8bdcbfcbb9f9da10
@rafaelgtvc <rafaelgtvc@gmail.com>
2020-08-05 23:06:23 -0300

    Inimigo 3 e pivot

--- Protect the body/assets/scripts/Enemie_3.gd
+++ Protect the body/assets/scripts/Enemie_3.gd
@@ -1,10 +1,8 @@
-
-extends KinematicBody2D
-
+extends Node2D
 
 export(float) var speed
 export(float) var Raio
-var vetor = Vector2()
+var vetor
 var acel = Vector2()
 var rng = RandomNumberGenerator.new()
 var resolution = OS.get_window_size()
@@ -12,58 +10,63 @@ var side
 var plain
 var pos = Vector2()
 var t = 0
-var perp
-var M 
-
+var M = Transform2D()
 func _ready():
+	
 	rng.seed = 300
 	rng.randomize()
-	
 	plain = rng.randi_range(1,4)
 	side = rng.randi_range(0,1)
 	match plain:
 		1:
 			if side == 0:
 				pos.x = rng.randf_range(0,resolution.x/2.0)
-				pos.y = -20
+				pos.y = -10
 			else:
-				pos.x = -20
+				pos.x = -10
 				pos.y = rng.randf_range(0,resolution.y/2.0)
 		2:
 			if side == 0:
 				pos.x = rng.randf_range(resolution.x/2.0,resolution.x)
-				pos.y = -20
+				pos.y = -10
 			else:
-				pos.x = resolution.x + 20.0
+				pos.x = resolution.x + 10.0
 				pos.y = rng.randf_range(resolution.y/2.0,resolution.y)
 		3:
 			if side == 0:
 				pos.x = rng.randf_range(0,resolution.x/2.0)
-				pos.y = resolution.y + 20
+				pos.y = resolution.y + 10
 			else:
-				pos.x = -20
+				pos.x = -10
 				pos.y = rng.randf_range(resolution.y/2.0,resolution.y)
 		4:
 			if side == 0:
 				pos.x = rng.randf_range(resolution.x/2.0,resolution.x)
-				pos.y = resolution.y + 20
+				pos.y = resolution.y + 10
 			else:
-				pos.x = resolution.x + 20
+				pos.x = resolution.x + 10
 				pos.y = rng.randf_range(resolution.y/2.0,resolution.y)
 	
+	
+	print(resolution)
 	self.position = pos
+	
 	vetor = (self.position - resolution/2.0).normalized()
 	acel = vetor*(-speed)
-	perp = Vector2(-acel.y,acel.x)
-	M = Transform2D(acel,perp,pos)
-	
+	var ang = acel.angle()
+	print("tg = ",tan(ang))
+	M = Transform2D(Vector2(cos(ang),-sin(ang)),Vector2(sin(ang),cos(ang)),self.position)
+	print("pos = ",pos)
+	print("acel = ",acel)
+	print("ang = ",ang)
+	print("vetor = ",vetor)
 	
 func _physics_process(delta):
-	var aux = Vector2(t,Raio*sin(t))
-	self.position.x += M[0].dot(aux)
-	self.position.y += M[1].dot(aux)
-	#self.position.y += t*acel.y
-	t+=delta
+	var parametric = Vector2(10.0*t,Raio*sin(t/2))
+	self.position.x = M[0].x * parametric.x + M[0].y * parametric.y + M[2].x
+	self.position.y = M[1].x * parametric.x + M[1].y * parametric.y + M[2].y
+	t+=delta*speed
+	print(position)
 
 
 

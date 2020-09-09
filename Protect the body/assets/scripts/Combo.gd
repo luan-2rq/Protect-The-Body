2ad9a97c1771686fb55c61a0bf542fe1484b944a
Otávio <otviofreitas@gmail.com>
2020-09-09 14:00:47 -0300

    Fix combo bugs

--- Protect the body/assets/scripts/Combo.gd
+++ Protect the body/assets/scripts/Combo.gd
@@ -2,6 +2,8 @@ extends Control
 
 var counter : int = 0
 var first : bool = true
+var combo_show : bool = false
+var mult_show : bool = false
 
 func _update_mult() -> void:
 	counter += 1
@@ -9,12 +11,15 @@ func _update_mult() -> void:
 	if first:
 		_appear($Combo_Text/AnimationPlayer)
 		first = false
+		combo_show = true
 	else:
 		_disappear($Multiplier/AnimationPlayer)
+		mult_show = false
 		yield($Multiplier/AnimationPlayer, "animation_finished")
 	
 	$Multiplier.text = ("x%s" % counter)
 	_appear($Multiplier/AnimationPlayer)
+	mult_show = true
 	
 	$Timer.start()
 
@@ -22,10 +27,16 @@ func _reset_mult() -> void:
 	counter = 0
 	first = true
 	
-	_disappear($Combo_Text/AnimationPlayer)
-	_disappear($Multiplier/AnimationPlayer)
-	yield($Multiplier/AnimationPlayer, "animation_finished")
-	$Multiplier.text = ("x%s" % counter)
+	if combo_show:
+		_disappear($Combo_Text/AnimationPlayer)
+		combo_show = false
+	
+	if mult_show:
+		_disappear($Multiplier/AnimationPlayer)
+		mult_show = false
+		yield($Multiplier/AnimationPlayer, "animation_finished")
+		$Multiplier.text = ("x%s" % counter)
+	else: $Multiplier.text = ("x%s" % counter)
 
 func _appear(child) -> void:
 	child.play("Appearing")

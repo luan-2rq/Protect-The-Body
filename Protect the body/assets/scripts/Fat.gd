8a24f17e107e5446f01348d64d2c3788c7bf4789
Gabriel Mota <gabrielfmota134@gmail.com>
2020-12-28 11:51:39 -0300

    Refactoring

--- Protect the body/assets/scripts/Fat.gd
+++ Protect the body/assets/scripts/Fat.gd
@@ -4,18 +4,18 @@ const maxLength = 512
 
 export var speed : int
 
+onready var pos : float = 0
 onready var LeftRight = [$Left, $Right]
 onready var UpDown = [$Up, $Down]
 
 func _physics_process(delta):
+	pos += speed * delta
 	for side in LeftRight:
-		side.get_node("RayCast2D").cast_to += Vector2(1, 0)* delta * speed
-		side.get_node("Sprite").region_rect.end.x = side.get_node("RayCast2D").cast_to.x
-		side.get_node("Area2D").scale.x = side.get_node("RayCast2D").cast_to.x / 512
+		side.get_node("Sprite").region_rect.end.x = pos
+		side.get_node("Area2D").scale.x = pos / 512
 	for side in UpDown:
-		side.get_node("RayCast2D").cast_to += Vector2(0, 1)* delta * speed * 360 / 512 
-		side.get_node("Sprite").region_rect.end.y = side.get_node("RayCast2D").cast_to.y
-		side.get_node("Area2D").scale.y = side.get_node("RayCast2D").cast_to.y / 360
+		side.get_node("Sprite").region_rect.end.y = pos * 360 / 512
+		side.get_node("Area2D").scale.y = pos / 360
 
 func _on_Area2D_body_entered(body):
 	print("FOI")
